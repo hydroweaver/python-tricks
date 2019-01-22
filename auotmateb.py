e15cce4611c1ffd858f21b364e212cd9fc5f0744
Verma <Karan.Verma@unilever.com>
2019-01-22 11:34:54 +0530

    python collection from python tricks however can only be used with python 2

---
+++ auotmateb.py
@@ -0,0 +1,7 @@
+i =10
+while i<=10:
+	print(i, 'We\'ll print till i is 6')
+	i -= 1
+	if i == 6:
+		print('i is 6 now, so breaking')
+		break
---
+++ collections.py
@@ -0,0 +1,6 @@
+## can only be used in python2 !! :|
+import collections
+
+c = collections.Counter('karan the king yo')
+
+print(c)
